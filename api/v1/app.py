16803474025ce8160f6ae04da8f27b74c5262f3e
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-01-07 23:00:34 +0000

    Handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Setting up my Flask application"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Teardown app context"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors by returning JSON format 404 response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
